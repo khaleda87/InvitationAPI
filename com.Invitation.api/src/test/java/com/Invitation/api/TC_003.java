388b8bb46053edd82267dd63318204d15347104f
Zia <chy_zia@yahoo.com>
2019-10-28 02:12:00 -0500

    Commit again

--- com.Invitation.api/src/test/java/com/Invitation/api/TC_003.java
+++ com.Invitation.api/src/test/java/com/Invitation/api/TC_003.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
+import io.restassured.authentication.PreemptiveBasicAuthScheme;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
@@ -12,9 +13,12 @@ public class TC_003 {
 	@Test
 	public void fetchInvite(){
 		RestAssured.baseURI = "http://authenticate.testapp.com:5030/Service";
+		String id = "12345abcde";
+		PreemptiveBasicAuthScheme aut = new PreemptiveBasicAuthScheme();
+		aut.setUserName(id);
 		String requestBody = "<AuthenticationRequestData>\r\n" + "<applicationId>123</applicationId>\r\n" + "<applicationPassword>Test123</applicationPassword>\r\n" +
 		                     "<clientIP>127.0.0.1</clientIP>\r\n" + "</AuthenticationRequestData>";
-		String id = "12345abcde";
+		
 		
 		Response response = null;
 		response =  RestAssured.given().
@@ -30,11 +34,6 @@ public class TC_003 {
 		
 		RestAssured.baseURI = "URI: https://testApplication.com/invitation";
 		
-		String requestBody1 = "<applicationData>\r\n" + "<appID>1234</appID>\r\n" + "<appName>Invitation</appName>r\\n\\" + 
-                "<appRole>provideID</appRole>\r\n" + "<appLocation>XXX</appLocation>\r\n" +"</applicationData>\r\n"
-	             + "<userName>abcde</userName>\r\n" + "<userEmail>abc@gmail.com</userEmail>\r\n" + "</userData>\r\n" +
-                "<inviteOption>\r\n" + "<inviteExpiry>10-26-2019</inviteExpiry>\r\n" + "</invitationRequest>";
-
 		
 		Response response1 = null;
 		response1 = RestAssured.given()
@@ -42,9 +41,8 @@ public class TC_003 {
 				    .oauth2(token)
 				    .contentType(ContentType.XML)
 				    .contentType(ContentType.XML)
-				    .body(requestBody1)
 				    .when()
-				    .get("/{ID}");
+				    .get("/fetch/{ID}");
 		
 		int statusCode = response1.getStatusCode();
 		Assert.assertEquals(statusCode, 200);
--- com.Invitation.api/src/test/java/com/Invitation/api/TC_004.java
+++ com.Invitation.api/src/test/java/com/Invitation/api/TC_004.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
+import io.restassured.authentication.PreemptiveBasicAuthScheme;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
@@ -12,9 +13,12 @@ public class TC_004 {
 	@Test
 	public void deleteInvite(){
 		RestAssured.baseURI = "http://authenticate.testapp.com:5030/Service";
+		String id = "12345abcde";
+		PreemptiveBasicAuthScheme aut = new PreemptiveBasicAuthScheme();
+		aut.setUserName(id);
 		String requestBody = "<AuthenticationRequestData>\r\n" + "<applicationId>123</applicationId>\r\n" + "<applicationPassword>Test123</applicationPassword>\r\n" +
 		                     "<clientIP>127.0.0.1</clientIP>\r\n" + "</AuthenticationRequestData>";
-		String id = "12345abcde";
+		
 		
 		Response response = null;
 		response =  RestAssured.given().
@@ -30,10 +34,6 @@ public class TC_004 {
 		
 		RestAssured.baseURI = "URI: https://testApplication.com/invitation";
 		
-		String requestBody1 = "<applicationData>\r\n" + "<appID>1234</appID>\r\n" + "<appName>Invitation</appName>r\\n\\" + 
-                "<appRole>provideID</appRole>\r\n" + "<appLocation>XXX</appLocation>\r\n" +"</applicationData>\r\n"
-	             + "<userName>abcde</userName>\r\n" + "<userEmail>abc@gmail.com</userEmail>\r\n" + "</userData>\r\n" +
-                "<inviteOption>\r\n" + "<inviteExpiry>10-26-2019</inviteExpiry>\r\n" + "</invitationRequest>";
 
 		
 		Response response1 = null;
@@ -42,9 +42,8 @@ public class TC_004 {
 				    .oauth2(token)
 				    .contentType(ContentType.XML)
 				    .contentType(ContentType.XML)
-				    .body(requestBody1)
 				    .when()
-				    .post("/create");
+				    .delete("/delete");
 		
 		int statusCode = response1.getStatusCode();
 		Assert.assertEquals(statusCode, 200);
